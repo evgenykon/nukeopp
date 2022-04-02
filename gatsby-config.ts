76dc5410c7f7ea371207408ac90d8e4ffcb4dce2
Евгений Коноплев <evgeny.konoplev@gemotest.ru>
2022-04-02 09:48:16 +0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,18 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
+
+export default config;
